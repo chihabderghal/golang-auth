5a5e9a7e1cfbd9dfc4d977b87dcde1e1fa8ebdd7
Chihab Derghal <chihabderghal1@gmail.com>
2024-10-04 22:39:15 +0100

    Add Login function in user_controller.go

--- internal/controllers/user_controller.go
+++ internal/controllers/user_controller.go
@@ -1,15 +1,17 @@
 package controllers
 
 import (
+	"errors"
 	"github.com/chihabderghal/user-service/internal/auth"
 	"github.com/chihabderghal/user-service/internal/config"
 	"github.com/chihabderghal/user-service/pkg/models"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
-type User struct {
+type UserRegister struct {
 	Firstname  string `json:"firstName" validate:"required"`
 	Lastname   string `json:"lastName" validate:"required"`
 	Country    string `json:"country" validate:"required"`
@@ -18,11 +20,16 @@ type User struct {
 	IsVerified bool   `json:"isVerified"`
 }
 
+type UserLogin struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
+
 var validate = validator.New()
 
 func Register(c *fiber.Ctx) error {
 	// Get user info from body
-	var userBody User
+	var userBody UserRegister
 	if err := c.BodyParser(&userBody); err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"message": "invalid request body",
@@ -77,10 +84,46 @@ func Register(c *fiber.Ctx) error {
 	)
 }
 
-func Login(ctx *fiber.Ctx) error {
-	return nil
+func Login(c *fiber.Ctx) error {
+	// Get credentials
+	var userBody UserLogin
+	if err := c.BodyParser(&userBody); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "invalid request body",
+		})
+	}
+
+	// validate user info
+	if err := validate.Struct(&userBody); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "invalid request body",
+		})
+	}
+
+	// Check if the user exists
+	var user models.User
+	err := config.DB.Where("email = ?", userBody.Email).First(&user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"message": "authentication failed: invalid email or password",
+			})
+		}
+		return nil
+	}
+
+	// Create tokens
+	tokens := auth.Tokens{
+		AccessToken:  auth.GenerateAccessToken(user),
+		RefreshToken: auth.GenerateRefreshToken(user),
+	}
+
+	// Return tokens
+	return c.Status(fiber.StatusOK).JSON(
+		tokens,
+	)
 }
 
-func Refresh(ctx *fiber.Ctx) error {
+func Refresh(c *fiber.Ctx) error {
 	return nil
 }
