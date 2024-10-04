ca539cdffa3b5b0eb4ac37404c02fb1c749d9314
Chihab Derghal <chihabderghal1@gmail.com>
2024-10-04 20:48:37 +0100

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"github.com/chihabderghal/user-service/internal/config"
+	"github.com/chihabderghal/user-service/internal/controllers"
+	"github.com/gofiber/fiber/v2"
+	"log"
+	"os"
+)
+
+func init() {
+	config.LoadEnv()
+	config.DBConnector()
+	config.AutoMigrations()
+}
 
 func main() {
-	fmt.Println("Hello World")
+	app := fiber.New()
+	port := os.Getenv("PORT")
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"message": "Everything looks good",
+		})
+	})
+
+	app.Post("/api/register", controllers.Register)
+	app.Post("/api/login", controllers.Login)
+	app.Post("/api/refresh", controllers.Refresh)
+
+	log.Fatal(app.Listen(":" + port))
 }
