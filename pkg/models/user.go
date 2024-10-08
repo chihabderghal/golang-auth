733c43689595560dbacc61411362d048cf005857
Chihab Derghal <chihabderghal1@gmail.com>
2024-10-08 20:52:47 +0100

    Add VerificationToken foreignkey to User model

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -1,14 +1,16 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	ID         uint   `gorm:"primaryKey;autoIncrement"`
-	Picture    string `gorm:"size:255"`
-	FirstName  string `gorm:"size:255"`
-	LastName   string `gorm:"size:255"`
-	Email      string `gorm:"type:varchar(100);unique"`
-	IsVerified bool   `gorm:"default:false"`
-	Password   string `gorm:"size:255;default:nil"`
+	Picture           string            `gorm:"size:255"`
+	FirstName         string            `gorm:"size:255"`
+	LastName          string            `gorm:"size:255"`
+	Email             string            `gorm:"type:varchar(100);unique"`
+	IsVerified        bool              `gorm:"default:false"`
+	Password          string            `gorm:"size:255;default:nil"`
+	VerificationToken VerificationToken `gorm:"foreignKey:UserId;constraint:OnDelete:CASCADE"`
 }
