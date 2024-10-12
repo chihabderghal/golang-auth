87280f185709b6a102dce879ba616acf4eff75c9
Chihab Derghal <chihabderghal1@gmail.com>
2024-10-12 13:22:04 +0100

    Add checkPasswordHash() to hash_helper.go

--- pkg/utils/hash_helper.go
+++ pkg/utils/hash_helper.go
@@ -4,9 +4,29 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// HashString hashes a given string using bcrypt.
+//
+// Parameters:
+//   - s: The string to be hashed.
+//
+// Returns:
+//   - A hashed string and an error. Returns an error if hashing fails.
 func HashString(s string) (string, error) {
 	// hash the password
 	hash, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.DefaultCost)
 
 	return string(hash), err
+}
+
+// CheckPasswordHash compares a plaintext password with a hashed password
+//
+// Parameters:
+// - password: The plaintext password to be verified.
+// - hash: The hashed password to compare against.
+//
+// Returns:
+// - bool: True if the password matches the hash, false if it does not.
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
