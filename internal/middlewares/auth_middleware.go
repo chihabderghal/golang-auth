b13e5da8ffc5ed893b9eb118ec6562b082361269
Chihab Derghal <chihabderghal1@gmail.com>
2024-10-13 20:11:30 +0100

    Add Protected middleware

--- internal/middlewares/auth_middleware.go
+++ internal/middlewares/auth_middleware.go
@@ -6,7 +6,14 @@ import (
 	"os"
 )
 
-// Protected protect routes
+// Protected is a middleware function that protects routes by ensuring
+// the user is authenticated. It checks for a JWT token in the cookies,
+// parses the token to validate the user's claims, and sets the user ID
+// and email in the context for further processing.
+//
+// Returns:
+// - fiber.Handler: A Fiber middleware handler that checks for authentication.
+// If the token is missing or invalid, it responds with a 401 Unauthorized status.
 func Protected() fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		token := c.Cookies("token")
