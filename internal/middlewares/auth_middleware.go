a266091a4b3544786bd389f7ba6b9a74676c1852
Chihab Derghal <chihabderghal1@gmail.com>
2024-10-14 10:06:22 +0100

    Add RequireAdmin() middleware in auth_middleware.go

--- internal/middlewares/auth_middleware.go
+++ internal/middlewares/auth_middleware.go
@@ -1,8 +1,12 @@
 package middlewares
 
 import (
+	"errors"
+	"github.com/chihabderghal/user-service/config"
+	"github.com/chihabderghal/user-service/pkg/models"
 	"github.com/chihabderghal/user-service/pkg/utils"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 	"os"
 )
 
@@ -10,13 +14,9 @@ import (
 // the user is authenticated. It checks for a JWT token in the cookies,
 // parses the token to validate the user's claims, and sets the user ID
 // and email in the context for further processing.
-//
-// Returns:
-// - fiber.Handler: A Fiber middleware handler that checks for authentication.
-// If the token is missing or invalid, it responds with a 401 Unauthorized status.
 func Protected() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		token := c.Cookies("token")
+		token := c.Cookies("accessToken")
 		if token == "" {
 			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
 				"error": "Unauthorized",
@@ -32,6 +32,45 @@ func Protected() fiber.Handler {
 
 		c.Locals("userId", claims["sub"])
 		c.Locals("email", claims["email"])
+
+		return c.Next()
+	}
+}
+
+// RequireAdmin is a middleware function that checks if the current user is an admin.
+// It retrieves the access token from cookies, validates the token, and checks the user's role.
+func RequireAdmin() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		token := c.Cookies("accessToken")
+		if token == "" {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "Unauthorized",
+			})
+		}
+
+		claims, err := utils.ParseJwt(token, os.Getenv("AT_SECRET"))
+		if err != nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "unauthorized",
+			})
+		}
+
+		var user models.User
+		err = config.DB.Where("email = ?", claims["email"]).First(&user).Error
+		if err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+					"message": "authentication failed: invalid email or password",
+				})
+			}
+			return nil
+		}
+
+		if !user.IsAdmin {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "unauthorized",
+			})
+		}
 
 		return c.Next()
 	}
