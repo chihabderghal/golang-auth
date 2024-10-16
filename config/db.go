c56f7832f689f2b69795deb8889f90dd38d807f5
Chihab Derghal <chihabderghal1@gmail.com>
2024-10-16 17:45:34 +0100

    Change the database connection from a hardcoded URL to a Docker-based configuration.

--- config/db.go
+++ config/db.go
@@ -1,9 +1,11 @@
 package config
 
 import (
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"os"
+	"strconv"
 )
 
 var DB *gorm.DB
@@ -11,10 +13,25 @@ var DB *gorm.DB
 func DBConnector() {
 	var err error
 
-	dsn := os.Getenv("DATABASE_URL")
+	p := os.Getenv("DB_PORT")
+	port, err := strconv.ParseUint(p, 10, 32)
+
+	if err != nil {
+		panic("failed to parse database port")
+	}
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		port,
+	)
+
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		panic("failed to connect database")
+		panic(fmt.Sprintf("failed to connect to database: %v", err))
 	}
 }
